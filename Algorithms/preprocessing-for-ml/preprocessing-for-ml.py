70646e0725c186f705fa001e404bb7f35ad7f532
MAZimmermann <MAZimmermann26@gmail.com>
2018-06-26 09:29:58 -0400

    Added comments and cleaned up code

--- Algorithms/preprocessing-for-ml/preprocessing-for-ml.py
+++ Algorithms/preprocessing-for-ml/preprocessing-for-ml.py
@@ -16,10 +16,10 @@ from collections import counter
 
 def process_data_for_labels(ticker):
     
-    # number of days we're looking ahead
+    # How many days do we have in the future to make or lose __%
     hm_days = 7
     
-    # read the pdf we previously saved
+    # read the pdf we previously created/saved
     df = pd.read_csv('sp500_joined_closes.csv', index_col=0)
     
     # tolist() might not be necessary?
@@ -30,7 +30,9 @@ def process_data_for_labels(ticker):
     
     # Iterate through previous data up to number days specified
     for i in range(1, hm_days + 1):
+        # On day __ into the future, what is the value in percent change?
         df['{}_{}d'.format(ticker, i)] = (df[ticker].shift(-i) - df[ticker]) / df[ticker]
+        # Price __ days from now minus today's price, divided by today's price
 
     # Replace NA/NaN values with 0
     df.fillna(0, inplace=True)
