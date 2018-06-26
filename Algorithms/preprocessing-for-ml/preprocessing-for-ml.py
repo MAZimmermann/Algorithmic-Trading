d02f711c386e17817896a64b6a2a8374fa42e91f
MAZimmermann <MAZimmermann26@gmail.com>
2018-06-26 10:26:05 -0400

    Finished creating labels for machine learning

--- Algorithms/preprocessing-for-ml/preprocessing-for-ml.py
+++ Algorithms/preprocessing-for-ml/preprocessing-for-ml.py
@@ -8,11 +8,10 @@ Code replicated/inspired by Sentdex 'Python Programming for Finance'
 """
 
 """ import required packages (numpy, pandas, pickle) """
+from collections import Counter
 import numpy as np
 import pandas as pd
 import pickle
-
-from collections import counter
 
 def process_data_for_labels(ticker):
     
@@ -68,26 +67,30 @@ def buy_sell_hold(*args):
     return 0
 
 def extract_featuresets(ticker):
+    
+    # Call process_data_for_labels and pass the desired ticker as the paramter
     tickers, df = process_data_for_labels(ticker)
-    df['{}_target'.format(ticker)] = list(map(buy_sell_hold, 
-       df['{}_1d'.format(ticker, i)],
-       df['{}_2d'.format(ticker, i)],
-       df['{}_3d'.format(ticker, i)],
-       df['{}_4d'.format(ticker, i)],
-       df['{}_5d'.format(ticker, i)],
-       df['{}_6d'.format(ticker, i)],
-       df['{}_7d'.format(ticker, i)]
-       ))
-    vals = df['{}_target'.format(ticker, i)].values.tolist()
+    
+    # Generates new collumn with buy, sell, or hold
+    df['{}_target'.format(ticker)] = list(map(buy_sell_hold,
+                                               df['{}_1d'.format(ticker)],
+                                               df['{}_2d'.format(ticker)],
+                                               df['{}_3d'.format(ticker)],
+                                               df['{}_4d'.format(ticker)],
+                                               df['{}_5d'.format(ticker)],
+                                               df['{}_6d'.format(ticker)],
+                                               df['{}_7d'.format(ticker)]))
+    
+    vals = df['{}_target'.format(ticker)].values.tolist()
     str_vals = [str(i) for i in vals]
     print('Data spread:', Counter(str_vals))
     
     df.fillna(0, inplace=True)
-    df = df.replace([np.inf, -np.inf, np.nan])
+    df = df.replace([np.inf, -np.inf], np.nan)
     df.dropna(inplace=True)
     
     df_vals = df[[ticker for ticker in tickers]].pct_change()
-    df_vals = df_vals.replace([np.inf, -np.inf, 0])
+    df_vals = df_vals.replace([np.inf, -np.inf], 0)
     df_vals.fillna(0, inplace=True)
     
     # 'X' feature sets, 'y' labels
@@ -96,3 +99,4 @@ def extract_featuresets(ticker):
     
     return X, y, df
     
+extract_featuresets('XOM')
