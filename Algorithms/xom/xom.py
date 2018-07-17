92b7c806ef630b097ef8eac89bd396818a84d4f2
MAZimmermann <MAZimmermann26@gmail.com>
2018-07-17 13:59:14 -0400

    Found a new source that doesn't drop my connection, but the table text is generated via JavaScript, so bs4 is unable to locate P/E through any simple means

--- Algorithms/xom/xom.py
+++ Algorithms/xom/xom.py
@@ -15,25 +15,22 @@ import requests
 ticker = 'xom'
 
 # .upper() will change all the letters in 'ticker' to upercase (not sure if this is necessary)
-url = 'https://www.zacks.com/stock/chart/'+ticker.upper()+'/fundamental/pe-ratio-ttm'
+url = 'http://financials.morningstar.com/valuation/price-ratio.html?t='+ticker.upper()
 
 # Make get request to our custom url, store response in resp
-resp = requests.get(url, timeout=5)
+resp = requests.get(url)
 
 # Make new beautiful soup object
-soup = bs.BeautifulSoup(resp.text, "lxml")
+soup = bs.BeautifulSoup(resp.text, "html.parser")
 
-# This will grab descriptive section containing the current P/E
-section = soup.find('section', {'id': 'stock_comp_desc'})
+# This will grab the table element that lists the P/E ratio (and some other values)
+table = soup.find('table', {'id': 'currentValuationTable'})
 
-# zacks.com has a predictable format for listing a companies P/E
-start = 'trailing-twelve-months P/E of'
-end = 'compared to'
+print(soup)
 
-for ptag in section.findAll('p'):
-    if "trailing-twelve-months P/E of" not in ptag.text: 
+"""for row in table.findAll('tr')[1:]:
+    if 'Price/Earnings' not in row.text: 
         continue
     else:
         # We found the P/E :)
-        theGoods = ptag.text[ptag.text.find(start):ptag.text.find(end)]
-        print(ticker,"has a",theGoods)
+        print('Word hey')  """
