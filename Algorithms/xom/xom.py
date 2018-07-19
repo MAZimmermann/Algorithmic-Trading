43a0c01b6ce7f0770ab9d97ff6ac7b275b5b673a
MAZimmermann <MAZimmermann26@gmail.com>
2018-07-19 14:20:15 -0400

    Found easily accessible resource for stock data, updating ticker.py in maz-pyapps next

--- Algorithms/xom/xom.py
+++ Algorithms/xom/xom.py
@@ -23,9 +23,7 @@ resp = requests.get(url)
 # Make new beautiful soup object
 soup = bs.BeautifulSoup(resp.text, "html.parser")
 
-for entry in soup.findAll('small'):
-    if 'P/E Ratio' not in entry.text: 
-        continue
-    else:
-        # We found the P/E :)
-        print('Word hey')
+dataList = soup.find('ul', {'class': 'list list--kv list--col50'})
+
+for dataListItem in dataList.findAll('li', {'class': 'kv__item'}):
+    print(dataListItem.text)
